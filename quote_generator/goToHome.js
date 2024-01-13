a2e39d535354f3ce482abc6d192c53f808ee6ff2
shambhavi912 <p.shambhavi.01@gmail.com>
2024-01-13 15:32:21 +0800

    updates

---
+++ quote_generator/goToHome.js
@@ -0,0 +1,5 @@
+// Your JavaScript file (script.js)
+function goToHomePage() {
+    // Use window.location.href to navigate to the homepage.html in a different directory
+    window.location.href = 'cs-portfolio-website/homepage.html';
+}
